0c31bf2b547c9ea2e09dbf849907cc1343966f1d
Jarred Parr <jparr721@gmail.com>
2023-03-09 12:20:50 -0500

    fix spelling of name

--- js/student.js
+++ js/student.js
@@ -1,11 +1,11 @@
 var Student = {
   // please fill in your name and NetID
   // your NetID is the part of your email before @yale.edu
-  name: "Enoch Omale, Jared Parr",
+  name: "Enoch Omale, Jarred Parr",
   netID: "eo329, jp2844",
 };
 
-Student.updateHTML = function() {
+Student.updateHTML = function () {
   var studentInfo = this.name + " &lt;" + this.netID + "&gt;";
   document.getElementById("student").innerHTML = studentInfo;
 };
